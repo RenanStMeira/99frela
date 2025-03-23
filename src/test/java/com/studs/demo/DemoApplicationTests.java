672011c6c4d5583a22da17265fa56e48c24249cf
RenanStMeira <renan.meira25@gmail.com>
2025-03-23 19:22:00 -0300

    configure test

--- src/test/java/com/studs/demo/DemoApplicationTests.java
+++ src/test/java/com/studs/demo/DemoApplicationTests.java
@@ -5,7 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
-@ContextConfiguration(classes = {DemoApplication.class, TestConfig.class})
 class DemoApplicationTests {
 
 	@Test
