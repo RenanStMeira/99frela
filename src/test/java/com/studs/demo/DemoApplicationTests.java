28726d5ae8d5681c5d66a59d9fae8de41c334d28
RenanStMeira <renan.meira25@gmail.com>
2025-03-23 22:21:59 -0300

    feat: ajuste test

--- src/test/java/com/studs/demo/DemoApplicationTests.java
+++ src/test/java/com/studs/demo/DemoApplicationTests.java
@@ -2,9 +2,11 @@ package com.studs.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DemoApplicationTests {
 
 	@Test
